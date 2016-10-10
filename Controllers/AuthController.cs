fd7c16cbf1283f7b5b44035ed9de4b4676aafd9d
AD\TongJ <wind.TWD@gmail.com>
2016-10-09 18:22:24 -0700

    ASP.NET - Designing the Login View

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+
+
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+
+    }
+}
