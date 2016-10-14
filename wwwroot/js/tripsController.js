00acd86baeac0edeca790d039978ff63fa4fea66
WIND-GS70\Wind <test@test.com>
2016-10-14 15:20:38 -0700

    AngularJS - Hello ANgularJS and Your First Controller

---
+++ wwwroot/js/tripsController.js
@@ -0,0 +1,19 @@
+﻿// tripsController.js
+
+(function () {
+    
+    "use strict";
+
+    // Get the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    funciton tripsController() {
+        
+        var vm = this;
+
+        vm.name = "Jeff";
+
+    }
+
+})();
