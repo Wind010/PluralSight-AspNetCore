5fb80532db10e73ed8e4da88fe6126f966f1e79d
WIND-GS70\Wind <wind.TWD@gmail.com>
2016-10-14 18:32:00 -0700

    AngularJS - Handling Forms

--- wwwroot/js/tripsController.js
+++ wwwroot/js/tripsController.js
@@ -21,8 +21,10 @@
         }];
 
         vm.newTrip = {};
+
         vm.addTrip = function () {
-            alert(vm.newTrip.name);
+            vm.trips.push({ name: vm.newTrip.name, created: new Date() });
+            vm.newTrip = {};
         };
 
     }
