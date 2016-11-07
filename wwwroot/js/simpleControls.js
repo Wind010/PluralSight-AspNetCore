6bd5ed8364ac6ee8de39e6c222f9043768aaf578
WIND-GS70\Wind <wind.TWD@gmail.com>
2016-11-06 17:45:12 -0800

    Creating a Directive

---
+++ wwwroot/js/simpleControls.js
@@ -0,0 +1,20 @@
+﻿// simpleControls.js
+
+(function () {
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+
+    function waitCursor() {
+        return {
+            scope: {
+                show: "=displayWhen"
+            },
+            // Only Elements
+            restrict: "E", 
+            templateUrl: "/views/waitCursor.html"
+        };
+    }
+
+})();
